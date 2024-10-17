f013be6087c199ec5aea223718e1414d7aee036a
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-10-17 21:06:52 +0530

    updated signin,signup for user

--- backend/src/controllers/user/signup.ts
+++ backend/src/controllers/user/signup.ts
@@ -6,10 +6,11 @@ import { signUpBody } from "../../zod";
 
 export const UserSignup = async (req:any, res:any) => {
     const { success, error } = signUpBody.safeParse(req.body);
+    const errorMessages = error?.issues.map(issue => issue.message);
 
     if (!success) {
         return res.status(400).json({
-            message: "Invalid inputs. Please check your data.",
+            message: errorMessages,
             error: error?.issues,
         });
     }
@@ -37,7 +38,12 @@ export const UserSignup = async (req:any, res:any) => {
 
         const token = jwt.sign({ id: savedUser._id }, JWT_USER_PASSWORD as string);
 
-        return res.status(200).json({
+        return res.status(200).cookie("token",token,{
+            httpOnly:process.env.NODE_ENV==='development'?false : true,
+            secure:process.env.NODE_ENV !== "development",
+            sameSite:process.env.NODE_ENV==='development'?"lax" : "none",
+            path:"/"
+        }).json({
             message: "Signup successful",
             user: savedUser,
             token,
